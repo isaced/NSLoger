bc1ebb26cff06471698ed3f14d0f32a64cdf3879
isaced <isaced@163.com>
2014-02-28 16:56:50 +0800

    移除重构框架后的废代码

--- bbs/views.py
+++ bbs/views.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteger
 
 from models import Subject,Comment
-from django.contrib.auth.models import User
 
 def index(request):
     '''首页'''
@@ -20,23 +16,3 @@ def subject(request,subject_id):
         return render_to_response("bbs/subject.html",{'subject':subject,'reply_list':reply_list})
     else:
         return render_to_response("404.html")
-
-def user(request,user_id):
-    ''''用户信息'''
-    user = User.objects.get(id=user_id)
-    if user:
-        sbject_list = Subject.objects.all()
-        comment_list = Comment.objects.filter(user=user)
-    return render_to_response("bbs/user.html",{"user":user,"subject_list":sbject_list,"comment_list":comment_list})
-
-def login(request):
-    '''登陆'''
-    return HttpResponse("login")
-
-def register(request):
-    '''用户注册'''
-    return HttpResponse("register")
-
-def logout(request):
-    '''注销'''
-    return HttpResponse("logout")
