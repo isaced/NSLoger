d6f8876a6eea5ebb422fc15d251e7898de06afea
cloverstd <cloverstd@gmail.com>
2014-03-02 22:02:40 +0800

    bug fixed

--- people/urls.py
+++ people/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import patterns, url
 from people import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.hello, name="index"),
         url(r'^register/$', views.register, name="register"),
         url(r'^login/$', views.login, name="login"),
         url(r'^logout/$', views.logout, name="logout"),
