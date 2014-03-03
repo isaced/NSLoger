247b2d51f526f9dd83c62fa2dfc4d6e9e2482cbd
isaced <isaced@163.com>
2014-03-03 17:01:51 +0800

    写了一点Admin

--- bbs/admin.py
+++ bbs/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from bbs.models import Topic,Comment,Category,Node
 
-admin.site.register(Topic)
-admin.site.register(Comment)
-admin.site.register(Category)
-admin.site.register(Node)
+class TopicAdmin(admin.ModelAdmin):
+	list_display = ('title','created_on','node','author','num_comments','num_views')
+	search_fields = ['title']
+	list_filter = ('node__name',)
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('content', 'topic', 'author','created_on',)
+	list_filter = ('topic__node__name',)
+
+class NodeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug' , 'category' ,'created_on', 'updated_on', 'num_topics')
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+
+
+admin.site.register(Topic,TopicAdmin)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Node,NodeAdmin)
--- bbs/models.py
+++ bbs/models.py
@@ -7,18 +7,18 @@ from people.models import Member as User
 # --- Model Define ---
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,verbose_name='类别名称')
 
     def __unicode__(self):
         return self.name
 
 class Node(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(blank=True, null=True,auto_now=True)
-    num_topics = models.IntegerField(default=0)
-    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=100,verbose_name='节点名称')
+    slug = models.SlugField(max_length=100,verbose_name='url标识符')
+    created_on = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
+    updated_on = models.DateTimeField(blank=True, null=True,auto_now=True,verbose_name='更新时间')
+    num_topics = models.IntegerField(default=0,verbose_name='主题数')
+    category = models.ForeignKey(Category,verbose_name='所属类别')
 
     def __unicode__(self):
         return self.name
@@ -30,7 +30,7 @@ class Topic(models.Model):
     author = models.ForeignKey(User,verbose_name='作者')
     num_views = models.IntegerField(default=0,verbose_name='浏览量')
     num_comments = models.IntegerField(default=0,verbose_name='评论数')
-    last_reply = models.ForeignKey(User,related_name='+')
+    last_reply = models.ForeignKey(User,related_name='+',verbose_name='最后回复者')
     datetime = models.DateTimeField(auto_now_add=True,verbose_name='发表时间')
     created_on = models.DateTimeField(auto_now_add=True,verbose_name='更新时间')
     updated_on = models.DateTimeField(auto_now=True,blank=True, null=True)
@@ -41,7 +41,7 @@ class Topic(models.Model):
 class Comment(models.Model):
     content = models.TextField(verbose_name='内容')
     author = models.ForeignKey(User,verbose_name='作者')
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic,verbose_name='所属主题')
     created_on = models.DateTimeField(auto_now_add=True,verbose_name='评论时间')
 
     def __unicode__(self):
