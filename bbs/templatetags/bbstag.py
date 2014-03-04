c085e59ff820519a649e94b4395de065b7f81601
isaced <isaced@163.com>
2014-03-04 16:54:16 +0800

    主题内容和评论添加Markdown支持

--- bbs/templatetags/bbstag.py
+++ bbs/templatetags/bbstag.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from django import template
+from django.utils.encoding import force_unicode
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 from bbs.models import Notice
+import markdown2
 
 register = template.Library()
+
+# --- tag ---
 
 @register.simple_tag
 def notice_set_all_readed(user):
@@ -13,3 +19,10 @@ def notice_set_all_readed(user):
 def num_notice(user):
     num = Notice.objects.filter(to_user=user,is_readed=False,is_deleted=False).count()
     return num
+
+# --- filter ---
+
+@register.filter(is_safe=True)
+@stringfilter
+def my_markdown(value):
+	return mark_safe(markdown2.markdown(force_unicode(value),extras=['fenced-code-blocks']))
