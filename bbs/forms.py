c695884480362a319b078bf79c14e0a4cfd68438
isaced <isaced@163.com>
2014-03-05 15:51:52 +0800

    完善发帖表单验证

--- bbs/forms.py
+++ bbs/forms.py
@@ -4,15 +4,31 @@ from django import forms
 from bbs.models import Topic, Comment
 
 class TopicForm(forms.ModelForm):
-    title = forms.CharField(label=u'标题',min_length=1,widget=forms.TextInput(attrs={'class':'form-control'}))
-    content = forms.CharField(label=u'内容',max_length=1000,min_length=1,widget=forms.Textarea(attrs={'class':'form-control','rows':'10'}))   
+    title = forms.CharField(label=u'标题',required=False)
+    content = forms.CharField(label=u'内容',required=False)
     
     def clean_title(self):
         title = self.cleaned_data.get("title").strip()
-        if title == '' or title == None:
-            raise forms.ValidationError(u"邮箱 %s 已经存在" % email)
+        if len(title) == 0:
+            raise forms.ValidationError("请输入标题...")
+        elif len(title) < 5:
+            raise forms.ValidationError("标题太短哦...")
+        elif len(title) > 100:
+            raise forms.ValidationError("标题太长哦...")
         else:
-            return title
+            return title   
+
+    def clean_content(self):
+        content = self.cleaned_data.get("content").strip()
+        if len(content) == 0:
+            raise forms.ValidationError("请输入内容...")
+        elif len(content) < 10:
+            raise forms.ValidationError("正文内容太短哦...")
+        elif len(content) > 2000:
+            raise forms.ValidationError("正文内容太长哦...")
+        else:
+            return content   
+                 
     class Meta:
         model = Topic
         fields = ('title','content')
