1311c8be7bab880d0a7242c6b0dbc4801926d30b
isaced <isaced@163.com>
2014-03-10 15:12:50 +0800

    修复Title消息提醒数显示问题

--- bbs/templatetags/bbstag.py
+++ bbs/templatetags/bbstag.py
@@ -20,7 +20,7 @@ def notice_set_all_readed(user):
     Notice.objects.filter(to_user=user,is_readed=False,is_deleted=False).update(is_readed=True)
     return ''
 
-@register.assignment_tag#(takes_context=True)
+@register.assignment_tag
 def num_notice(user):
     num = Notice.objects.filter(to_user=user,is_readed=False,is_deleted=False).count()
     return num
