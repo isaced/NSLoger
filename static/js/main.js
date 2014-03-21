33c973ef7c3a88c1b710fc8179adb3fbd9d07b29
isaced <isaced@163.com>
2014-03-21 10:54:56 +0800

    给提交按钮添加loading状态

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,15 @@
-// Close Message
-$("button#message-close").click(function () {
-  $("button#message-close").parent().hide("slow");
+/*
+ * 初始化按钮的loading功能，点击后将显示Loading字样，并且按钮被disabled掉，无法连续点击，防止二次提交
+ * 超过3秒后按钮将恢复原状
+ */
+$(function(){
+	$('button[data-loading-text]').click(function () {
+	    var btn = $(this).button('loading');
+	    setTimeout(function () {
+	        btn.button('reset');
+	    }, 3000);
+	});
 });
-
-// no re submit
-// $("form").submit(function(){  
-// $(":submit",this).attr("disabled","disabled");  
-// });
 
 // Reply Comment
 function replyOne(username,at_floor){
